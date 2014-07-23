d72d95821f9197abf8fbca92f19e798e6d3f1a8e
mhong777 <mike.sj.hong@gmail.com>
2014-07-22 20:51:45 -0400

    add headers

--- config/express.js
+++ config/express.js
@@ -135,6 +135,20 @@ module.exports = function(db) {
 			error: 'Not Found'
 		});
 	});
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 	return app;
 };
