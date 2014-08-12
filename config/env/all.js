b5464f1e10a6ca255271d66887aa535804cadc7f
mhong777 <mike.sj.hong@gmail.com>
2014-08-11 20:09:09 -0400

    try change all.js

--- config/env/all.js
+++ config/env/all.js
@@ -23,7 +23,7 @@ module.exports = {
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
                 'public/lib/socket.io-client/socket.io.js',
-                'public/lib/angular-socket-io/socket.js'
+                'public/lib/angular-socket-io/socket.js' 
 			]
 		},
 		css: [
